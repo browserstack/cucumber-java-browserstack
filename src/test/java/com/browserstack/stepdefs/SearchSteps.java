5f757780a09557f09e30b4c66f939fac73b2d3de
Assaf Shlomi <assaf.shlomi@broadcom.com>
2021-12-23 16:39:54 +0200

    Update SearchSteps.java

--- src/test/java/com/browserstack/stepdefs/SearchSteps.java
+++ src/test/java/com/browserstack/stepdefs/SearchSteps.java
@@ -23,7 +23,14 @@ public class SearchSteps {
     private WebDriver driver;
     private SearchPage searchPage;
     private Local l;
+    
+//Assaf
+    @RunWith(Cucumber.class)
+    @CucumberOptions()
 
+    public class RunnerTest {
+    }
+//Assaf
     @Before
     public void setUp(Scenario scenario) throws Exception {
         String USERNAME = System.getenv("BROWSERSTACK_USERNAME");
